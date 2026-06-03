f7cde8d1a67f0e79922e50365562ba9716df562b
Горбунова Дарья <damagorbunova@edu.hse.ru>
2026-06-03 19:33:31 +0300

    Реализован класс Recipe

--- main.py
+++ main.py
@@ -23,3 +23,45 @@ class Ingredient:
     
     def __eq__(self, other):
         return self.name==other.name and self.unit==other.unit
+
+ 
+class Recipe:
+    def __init__(self, title: str, ingredients):
+        self.title=title
+        self.ingredients=ingredients
+
+    def add_ingredient(self, ingredient: Ingredient):
+        for i in self.ingredients:
+            if i==ingredient:
+                i.quantity+=ingredient.quantity
+                return
+        self.ingredients.append(ingredient)
+
+    @staticmethod
+    def is_valid_ratio(ratio):
+        if isinstance(ratio, (int, float)):
+            if ratio>0:
+                return True
+        return False
+
+    def scale(self, ratio: float):
+        if self.is_valid_ratio(ratio): #в задании чётко не прописана надбность проверки, но наврное метод для этого
+            ingredients_1=[]
+            for i in self.ingredients:
+                ingredient_1=Ingredient(i.name, i.quantity*ratio, i.unit)
+                ingredients_1.append(ingredient_1)
+            return Recipe(self.title, ingredients_1)
+        else:
+            raise ValueError("Ratio должно быль положительным")
+
+    def __len__(self):
+        return len(self.ingredients)
+
+    def __str__(self):
+        s= f'{self.title}\n'
+        s+=f'Список ингридиентов\n'
+        for i in self.ingredients:
+            s+=f'{i}\n'
+        return s
+    
+    
